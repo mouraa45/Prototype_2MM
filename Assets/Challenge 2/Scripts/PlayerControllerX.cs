23912cf7318895c425ba3d9aa3f987fb9b7fb3fc
mouraa45 <a45829@alunos.ipb.pt>
2023-05-05 16:17:12 +0100

    passo 9
    jogador gera um novo cão após um certo período de tempo

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private float coolDown =1;
 
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space)&& coolDown<=0) 
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            coolDown=1;
+            
+        }
+        
+        if(coolDown>0){
+        coolDown-=Time.deltaTime;
         }
     }
 }
